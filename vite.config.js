64d871d99777c7efcf080ddde020f6b4928cfc31
fediakin <fedrvoynr@gmail.com>
2024-12-18 12:39:51 +0200

    commit

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig(({ command }) => {
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
+    base: '/goit-js-hw-09/', // Додайте цей рядок
     root: 'src',
     build: {
       sourcemap: true,
